102b06a6c2360cab9d0b5af5b88ed64870cac3eb
지의신 <64007655+usgeeus@users.noreply.github.com>
2023-10-16 13:08:54 +0900

    J_25.Candidate add onlyOwner on initialize func

--- contracts/dao/Candidate.sol
+++ contracts/dao/Candidate.sol
@@ -55,7 +55,7 @@ contract Candidate is ProxyStorage, AccessibleCommon, CandidateStorage, ILayer2 
         string memory _memo,
         address _committee,
         address _seigManager
-    ) external  {
+    ) external onlyOwner  {
         require(
             _candidate != address(0)
             || _committee != address(0)
