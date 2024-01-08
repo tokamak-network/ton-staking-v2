d49a3b8caf5c9102e221cea6ee49d6d7fe8665e3
HyukSang <zzooppii@gmail.com>
2024-01-08 15:30:25 +0900

    Z_Audit_Deploy

--- scripts/1.deploy_DAO.js
+++ scripts/1.deploy_DAO.js
@@ -17,9 +17,18 @@ async function DeployDAOCommitteeOwner() {
     console.log('daoCommitteeOwnerLogic' , daoCommitteeOwnerLogic.address)
 }
 
+async function DeployDAOCommittee_V1() {
+    //==== DAOCommitteeOwner =================================
+    const DAOCommittee_V1Dep = await ethers.getContractFactory("DAOCommittee_V1");
+    const DAOCommittee_V1 = await DAOCommittee_V1Dep.deploy();
+    await DAOCommittee_V1.deployed();
+    console.log('DAOCommittee_V1' , DAOCommittee_V1.address)
+}
+
 const main = async () => {
   await DeployDAOCommitteeDAOVault()
   await DeployDAOCommitteeOwner()
+  await DeployDAOCommittee_V1()
 }
 
 
