c82b81581c8e886f2d1e459778749f4de4e56f34
Zena-park <zena@tokamak.network>
2024-06-13 13:55:38 +0900

    feat: extract the used gas log from the receipt #11

---
+++ test/shared/logUtils.ts
@@ -0,0 +1,58 @@
+import xlsx from 'xlsx'
+import path from 'path'
+import { ethers } from 'hardhat'
+
+export function gasUsedFunctions (_contract: string, _fun: string, _detail: string, _tx:any) {
+    let data = {
+        "Contract": _contract,
+        "Name": _fun,
+        "Tx": '',
+        "Description": _detail,
+        "GasUsed": '',
+        };
+
+    if( _tx != null && (
+        (_tx.deployTransaction != null && _tx.deployTransaction.hash != null)
+        || _tx.hash !=null || _tx.transactionHash !=null )
+      ) {
+
+        let hash = _tx.hash;
+
+        if(_tx.deployTransaction != null && _tx.deployTransaction.hash!=null) hash = _tx.deployTransaction.hash;
+        if(_tx.transactionHash != null && _tx.transactionHash!=null) hash = _tx.transactionHash;
+        data.Tx = hash
+
+        if (_tx.gasUsed != null) data.GasUsed = _tx.gasUsed.toString()
+    }
+    return data
+  }
+
+  export const exportLogsToExcel = (transactions: Array<any>, workSheetColumnNames: any, workSheetName: any, filePath:string, gasPrice: number) => {
+    const data = transactions.map(transaction => {
+        let usd = transaction.GasUsed * gasPrice
+        return [
+            transaction.Contract,
+            transaction.Name,
+            '',
+            transaction.Description,
+            transaction.GasUsed,
+            ethers.utils.formatEther(ethers.BigNumber.from(""+usd).mul(ethers.BigNumber.from("1000000000")))
+            ];
+    });
+    exportExcel(data, workSheetColumnNames, workSheetName, filePath);
+}
+
+const exportExcel = (data: any, workSheetColumnNames: any, workSheetName:string, filePath:string) => {
+    const workBook = xlsx.utils.book_new();
+    const workSheetData = [
+        workSheetColumnNames,
+        ...data
+    ];
+
+    //console.log(workSheetData);
+
+    const workSheet = xlsx.utils.aoa_to_sheet(workSheetData);
+    xlsx.utils.book_append_sheet(workBook, workSheet, workSheetName);
+    xlsx.writeFile(workBook, path.resolve(filePath));
+}
+
