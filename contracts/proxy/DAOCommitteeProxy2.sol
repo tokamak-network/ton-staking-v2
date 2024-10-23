4701a20ffaa42e4d6cb1b76fa1ed64fa5e9f0f2b
Zena-park <zena@tokamak.network>
2024-10-23 23:23:23 +0900

    style: keep code within 100 Columns to improve readability #58
    - /contracts/stake/managers/SeigManagerV1_3.sol:
    - /contracts/stake/managers/DepositManagerV1_1.sol:
    - /contracts/proxy/DAOCommitteeProxy2.sol:

--- contracts/proxy/DAOCommitteeProxy2.sol
+++ contracts/proxy/DAOCommitteeProxy2.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "../dao/StorageStateCommittee.sol";
-import {AccessControl} from "../accessControl/AccessControl.sol";
-import {ERC165A}  from "../accessControl/ERC165A.sol";
-import "./ProxyStorage2.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import '../dao/StorageStateCommittee.sol';
+import {AccessControl} from '../accessControl/AccessControl.sol';
+import {ERC165A} from '../accessControl/ERC165A.sol';
+import './ProxyStorage2.sol';
+import {Address} from '@openzeppelin/contracts/utils/Address.sol';
 
-import "../interfaces/IProxyEvent.sol";
-import "../interfaces/IProxyAction2.sol";
+import '../interfaces/IProxyEvent.sol';
+import '../interfaces/IProxyAction2.sol';
 
-contract DAOCommitteeProxy2 is 
+contract DAOCommitteeProxy2 is
     StorageStateCommittee,
     AccessControl,
-    ERC165A, 
+    ERC165A,
     ProxyStorage2,
-    IProxyEvent, 
+    IProxyEvent,
     IProxyAction2
 {
     address internal _implementation;
@@ -25,25 +25,23 @@ contract DAOCommitteeProxy2 is
         // console.log("msg.sedner :", msg.sender);
         // console.logBytes32(DEFAULT_ADMIN_ROLE);
         // console.logBool(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "DAOCommitteeProxy2: msg.sender is not an admin");
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            'DAOCommitteeProxy2: msg.sender is not an admin'
+        );
         _;
     }
-
 
     /* ========== onlyOwner ========== */
 
     /// @notice Set implementation contract
     /// @param impl New implementation contract address
     function upgradeTo2(address impl) external onlyOwner2 {
-        require(impl != address(0), "input is zero");
-        require(
-            _implementation2(0) != impl,
-            "same addr"
-        );
+        require(impl != address(0), 'input is zero');
+        require(_implementation2(0) != impl, 'same addr');
         _setImplementation2(impl, 0, true);
         emit Upgraded(impl);
     }
-
 
     /// @inheritdoc IProxyAction2
     function setImplementation2(
@@ -55,9 +53,10 @@ contract DAOCommitteeProxy2 is
     }
 
     /// @inheritdoc IProxyAction2
-    function setAliveImplementation2(address newImplementation, bool _alive)
-        public override onlyOwner2
-    {
+    function setAliveImplementation2(
+        address newImplementation,
+        bool _alive
+    ) public override onlyOwner2 {
         _setAliveImplementation2(newImplementation, _alive);
     }
 
@@ -66,25 +65,17 @@ contract DAOCommitteeProxy2 is
         bytes4[] calldata _selectors,
         address _imp
     ) public override onlyOwner2 {
-        require(
-            _selectors.length > 0,
-            "Proxy: _selectors's size is zero"
-        );
-        require(aliveImplementation[_imp], "Proxy: _imp is not alive");
+        require(_selectors.length > 0, "Proxy: _selectors's size is zero");
+        require(aliveImplementation[_imp], 'Proxy: _imp is not alive');
 
         for (uint256 i = 0; i < _selectors.length; i++) {
-            require(
-                selectorImplementation[_selectors[i]] != _imp,
-                "Proxy: same imp"
-            );
+            require(selectorImplementation[_selectors[i]] != _imp, 'Proxy: same imp');
             selectorImplementation[_selectors[i]] = _imp;
             emit SetSelectorImplementation(_selectors[i], _imp);
         }
     }
 
     /* ========== Anyone can   ========== */
-
-
 
     /* ========== VIEW ========== */
 
@@ -94,29 +85,23 @@ contract DAOCommitteeProxy2 is
     }
 
     /// @inheritdoc IProxyAction2
-    function implementation2(uint256 _index) external override view returns (address) {
+    function implementation2(uint256 _index) external view override returns (address) {
         return _implementation2(_index);
     }
 
-
     /// @inheritdoc IProxyAction2
-    function getSelectorImplementation2(bytes4 _selector)
-        public override
-        view
-        returns (address impl)
-    {
-        if (selectorImplementation[_selector] == address(0))
-            return proxyImplementation[0];
-        else if (aliveImplementation[selectorImplementation[_selector]]){
+    function getSelectorImplementation2(
+        bytes4 _selector
+    ) public view override returns (address impl) {
+        if (selectorImplementation[_selector] == address(0)) return proxyImplementation[0];
+        else if (aliveImplementation[selectorImplementation[_selector]]) {
             return selectorImplementation[_selector];
-        }
-        else return proxyImplementation[0];
+        } else return proxyImplementation[0];
     }
-
 
     /// @dev receive ether
     receive() external payable {
-        revert("cannot receive Ether");
+        revert('cannot receive Ether');
     }
 
     /// @dev fallback function , execute on undefined function call
@@ -129,11 +114,7 @@ contract DAOCommitteeProxy2 is
     /// @dev view implementation address of the proxy[index]
     /// @param _index index of proxy
     /// @return impl address of the implementation
-    function _implementation2(uint256 _index)
-        internal
-        view
-        returns (address impl)
-    {
+    function _implementation2(uint256 _index) internal view returns (address impl) {
         return proxyImplementation[_index];
     }
 
@@ -141,10 +122,7 @@ contract DAOCommitteeProxy2 is
     function _fallback() internal {
         address _impl = getSelectorImplementation2(msg.sig);
 
-        require(
-            _impl != address(0) && !pauseProxy,
-            "Proxy: impl OR proxy is false"
-        );
+        require(_impl != address(0) && !pauseProxy, 'Proxy: impl OR proxy is false');
 
         assembly {
             // Copy msg.data. We take full control of memory in this inline assembly
@@ -160,13 +138,13 @@ contract DAOCommitteeProxy2 is
             returndatacopy(0, 0, returndatasize())
 
             switch result
-                // delegatecall returns 0 on error.
-                case 0 {
-                    revert(0, returndatasize())
-                }
-                default {
-                    return(0, returndatasize())
-                }
+            // delegatecall returns 0 on error.
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
         }
     }
 
@@ -174,15 +152,8 @@ contract DAOCommitteeProxy2 is
     /// @param newImplementation Address of the new implementation.
     /// @param _index index of proxy
     /// @param _alive alive status
-    function _setImplementation2(
-        address newImplementation,
-        uint256 _index,
-        bool _alive
-    ) internal {
-        require(
-            Address.isContract(newImplementation),
-            "Proxy: not contract address"
-        );
+    function _setImplementation2(address newImplementation, uint256 _index, bool _alive) internal {
+        require(Address.isContract(newImplementation), 'Proxy: not contract address');
         if (_alive) proxyImplementation[_index] = newImplementation;
         _setAliveImplementation2(newImplementation, _alive);
     }
@@ -190,11 +161,8 @@ contract DAOCommitteeProxy2 is
     /// @dev set alive status of implementation
     /// @param newImplementation Address of the new implementation.
     /// @param _alive alive status
-    function _setAliveImplementation2(address newImplementation, bool _alive)
-        internal
-    {
+    function _setAliveImplementation2(address newImplementation, bool _alive) internal {
         aliveImplementation[newImplementation] = _alive;
         emit SetAliveImplementation(newImplementation, _alive);
     }
-
 }
