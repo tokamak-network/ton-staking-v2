86e9a2079131d107757f911a316c38fd5d82a0db
Zena-park <zena@tokamak.network>
2025-01-22 18:12:56 +0900

    test SystemConfig

--- test/layer2/units/10.upgrade-thanos-systemConfig.sepolia.test.ts
+++ test/layer2/units/10.upgrade-thanos-systemConfig.sepolia.test.ts
@@ -278,7 +278,6 @@ describe('Upgrade Thanos sepolia', () => {
             console.log(newThanosContract.address)
             const thanosProxy: Contract = (await ethers.getContractAt(Proxy_Json, thanosSepoliaSystemConfig, thanosSepoliaProxyAdminSigner))
             const thanos: Contract = (await ethers.getContractAt(Thanos_Json.abi, thanosSepoliaSystemConfig, deployer))
-            await (await thanosProxy.connect(thanosSepoliaProxyAdminSigner).upgradeTo( newThanosContract.address)).wait()
 
             let l1CrossDomainMessenger_ = await thanos.l1CrossDomainMessenger()
             let l1ERC721Bridge_ = await thanos.l1ERC721Bridge()
@@ -299,37 +298,9 @@ describe('Upgrade Thanos sepolia', () => {
             console.log('nativeTokenAddress_', nativeTokenAddress_)
             console.log('batchInbox_', batchInbox_)
 
-            // const callDtata = newThanosContract.interface.encodeFunctionData(
-            //     "initialize(address,uint32,uint32,bytes32,uint64,address,(uint32,uint8,uint8,uint32,uint32,uint128),address,(address,address,address,address,address,address,address,address,address))",
-            //     [   thanosSepoliaProxyAdmin,
-            //         1368,
-            //         810949,
-            //         '0x00000000000000000000000061dc95e5f27266b94805ed23d95b4c9553a3d049',
-            //         200000000,
-            //         '0x0Fd5632f3b52458C31A2C3eE1F4b447001872Be9',
-            //         {
-            //             maxResourceLimit: 20000000,
-            //             elasticityMultiplier: 10,
-            //             baseFeeMaxChangeDenominator: 8,
-            //             minimumBaseFee: 1000000000,
-            //             systemTxMaxGas: 1000000,
-            //             maximumBaseFee: BigNumber.from('340282366920938463463374607431768211455')
-            //         },
-            //         batchInbox_,
-            //         {
-            //             l1CrossDomainMessenger: l1CrossDomainMessenger_,
-            //             l1ERC721Bridge: l1ERC721Bridge_,
-            //             l1StandardBridge: l1StandardBridge_,
-            //             disputeGameFactory: disputeGameFactory_,
-            //             optimismPortal: optimismPortal_,
-            //             optimismMintableERC20Factory: optimismMintableERC20Factory_,
-            //             gasPayingToken: gasPayingToken_[0],
-            //             nativeTokenAddress: nativeTokenAddress_,
-            //             seigniorageReceiver: deployer.address
-            //         }  ])
-
-            await (await thanos.connect(deployer).initialize(
-                thanosSepoliaProxyAdmin,
+            const callDtata = newThanosContract.interface.encodeFunctionData(
+                "initialize(address,uint32,uint32,bytes32,uint64,address,(uint32,uint8,uint8,uint32,uint32,uint128),address,(address,address,address,address,address,address,address,address,address))",
+                [   thanosSepoliaProxyAdmin,
                     1368,
                     810949,
                     '0x00000000000000000000000061dc95e5f27266b94805ed23d95b4c9553a3d049',
@@ -354,19 +325,23 @@ describe('Upgrade Thanos sepolia', () => {
                         gasPayingToken: gasPayingToken_[0],
                         nativeTokenAddress: nativeTokenAddress_,
                         seigniorageReceiver: deployer.address
-                    }
-            )).wait()
+                    }  ])
 
-            expect(await thanosProxy.l1CrossDomainMessenger()).to.be.eq(l1CrossDomainMessenger_)
-            expect(await thanosProxy.l1ERC721Bridge()).to.be.eq(l1ERC721Bridge_)
-            expect(await thanosProxy.l1StandardBridge()).to.be.eq(l1StandardBridge_)
-            expect(await thanosProxy.disputeGameFactory()).to.be.eq(disputeGameFactory_)
-            expect(await thanosProxy.optimismPortal()).to.be.eq(optimismPortal_)
-            expect(await thanosProxy.optimismMintableERC20Factory()).to.be.eq(optimismMintableERC20Factory_)
-            expect(await thanosProxy.gasPayingToken()).to.be.eq(gasPayingToken_)
-            expect(await thanosProxy.nativeTokenAddress()).to.be.eq(nativeTokenAddress_)
-            expect(await thanosProxy.batchInbox()).to.be.eq(nativeTokenAddress_)
-            expect(await thanosProxy.seigniorageReceiver()).to.be.eq(deployer.address)
+
+            await (await thanosProxy.connect(thanosSepoliaProxyAdminSigner).upgradeToAndCall(
+                 newThanosContract.address
+                , callDtata)).wait()
+
+            expect(await thanos.l1CrossDomainMessenger()).to.be.eq(l1CrossDomainMessenger_)
+            expect(await thanos.l1ERC721Bridge()).to.be.eq(l1ERC721Bridge_)
+            expect(await thanos.l1StandardBridge()).to.be.eq(l1StandardBridge_)
+            expect(await thanos.disputeGameFactory()).to.be.eq(disputeGameFactory_)
+            expect(await thanos.optimismPortal()).to.be.eq(optimismPortal_)
+            expect(await thanos.optimismMintableERC20Factory()).to.be.eq(optimismMintableERC20Factory_)
+            // expect(await thanos.gasPayingToken()).to.be.eq(gasPayingToken_)
+            expect(await thanos.nativeTokenAddress()).to.be.eq(nativeTokenAddress_)
+            expect(await thanos.batchInbox()).to.be.eq(batchInbox_)
+            expect(await thanos.seigniorageReceiver()).to.be.eq(deployer.address)
 
         })
     })
