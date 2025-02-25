b4d528dfcc02abe3ef4f11aa11734cd06fd19434
Zena-park <zena@tokamak.network>
2025-02-25 19:14:45 +0900

    feat: After setting ton, do not initialize (CandidateAddOnV1_1 the role DEFAULT_ADMIN_ROLE)

--- contracts/dao/CandidateAddOnV1_1.sol
+++ contracts/dao/CandidateAddOnV1_1.sol
@@ -46,6 +46,7 @@ contract CandidateAddOnV1_1 is
         address _ton,
         address _wton
     ) external onlyOwner  {
+        require(ton == address(0), "already initialized");
         require(
             _operateContract != address(0)
             || _committee != address(0)
