e6e1d4fb9ed8150629ee862292f6b4f277103c96
Zena-park <zena@tokamak.network>
2025-02-26 16:12:23 +0900

    fix: remove unnecessary verification code #65

--- contracts/dao/DAOCommittee_V1.sol
+++ contracts/dao/DAOCommittee_V1.sol
@@ -167,10 +167,10 @@ contract DAOCommittee_V1 is
             address(seigManager)
         );
 
-        require(
-            candidateContract != address(0),
-            "DAOCommittee: deployed candidateContract is zero"
-        );
+        // require(
+        //     candidateContract != address(0),
+        //     "DAOCommittee: deployed candidateContract is zero"
+        // );
         require(
             _candidateInfos[_operator].candidateContract == address(0),
             "DAOCommittee: The candidate already has contract"
@@ -210,10 +210,10 @@ contract DAOCommittee_V1 is
             address(seigManager)
         );
 
-        require(
-            candidateContract != address(0),
-            "DAOCommittee: deployed candidateContract is zero"
-        );
+        // require(
+        //     candidateContract != address(0),
+        //     "DAOCommittee: deployed candidateContract is zero"
+        // );
 
         if(_candidateInfos[_operatorAddress].candidateContract != address(0) ) {
 
@@ -264,7 +264,7 @@ contract DAOCommittee_V1 is
             address(this),
             address(seigManager)
         );
-        if (candidateContract == address(0)) revert CreateCandiateError(1);
+        // if (candidateContract == address(0)) revert CreateCandiateError(1);
         if (_candidateInfos[_operatorManagerAddress].candidateContract != address(0)) revert CreateCandiateError(2);
 
         _candidateInfos[_operatorManagerAddress] = CandidateInfo({
@@ -640,10 +640,10 @@ contract DAOCommittee_V1 is
             address(seigManager)
         );
 
-        require(
-            candidateContract != address(0),
-            "DAOCommittee: deployed candidateContract is zero"
-        );
+        // require(
+        //     candidateContract != address(0),
+        //     "DAOCommittee: deployed candidateContract is zero"
+        // );
 
         _candidateInfos[_layer2] = CandidateInfo({
             candidateContract: candidateContract,
--- contracts/dao/factory/CandidateAddOnFactory.sol
+++ contracts/dao/factory/CandidateAddOnFactory.sol
@@ -94,7 +94,6 @@ contract CandidateAddOnFactory is ProxyStorage, AccessibleCommon, CandidateAddOn
     {
         require(daoCommittee == _committee, "different daoCommittee");
         CandidateAddOnProxy c = new CandidateAddOnProxy();
-        require(address(c) != address(0), "zero CandidateAddOnProxy");
 
         c.upgradeTo(candidateAddOnImp);
         ICandidateAddOn(address(c)).initialize(
