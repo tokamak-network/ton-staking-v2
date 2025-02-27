b8435ae3bc08bdc9c1b715843e057ba84d25008d
Zena-park <zena@tokamak.network>
2025-02-27 12:51:31 +0900

    fix: TS-4 - Multiple init possible (CandidateAddOnV1_1)

--- contracts/dao/CandidateAddOnV1_1.sol
+++ contracts/dao/CandidateAddOnV1_1.sol
@@ -47,6 +47,13 @@ contract CandidateAddOnV1_1 is
         address _wton
     ) external onlyOwner  {
         require(
+            ton == address(0) &&
+            wton == address(0) &&
+            seigManager == address(0),
+            "already initialized"
+            );
+
+        require(
             _operateContract != address(0)
             || _committee != address(0)
             || _seigManager != address(0),
