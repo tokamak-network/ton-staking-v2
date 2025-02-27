9e6c1d9016cb7e2b7aa3f8d213b08fbc034e4582
Zena-park <zena@tokamak.network>
2025-02-27 18:46:50 +0900

    fix: TS-19 - Duplicate code in modifier (nonRejected)
    remove unused modifier nonRejected

--- contracts/layer2/L1BridgeRegistryV1_1.sol
+++ contracts/layer2/L1BridgeRegistryV1_1.sol
@@ -112,11 +112,6 @@ contract L1BridgeRegistryV1_1 is ProxyStorage, AuthControlL1BridgeRegistry, L1Br
         _;
     }
 
-    modifier nonRejected(address rollupConfig) {
-        require(!rollupInfo[rollupConfig].rejectedSeigs, "rejected");
-        _;
-    }
-
     /* ========== CONSTRUCTOR ========== */
     constructor() {}
 
