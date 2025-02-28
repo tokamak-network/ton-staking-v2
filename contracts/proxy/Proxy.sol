58a5863fb8585739605a5ab7546494c35e7ff845
Zena-park <zena@tokamak.network>
2025-02-28 23:47:47 +0900

    [PYX-01M] Improper Imposition of Restriction #119

--- contracts/proxy/Proxy.sol
+++ contracts/proxy/Proxy.sol
@@ -188,7 +188,9 @@ contract Proxy is ProxyStorage, AccessibleCommon, IProxyEvent, IProxyAction
     function _setAliveImplementation2(address newImplementation, bool _alive)
         internal
     {
-        aliveImplementation[newImplementation] = _alive;
+        if(_alive) aliveImplementation[newImplementation] = _alive;
+        else delete aliveImplementation[newImplementation];
+
         emit SetAliveImplementation(newImplementation, _alive);
     }
 
