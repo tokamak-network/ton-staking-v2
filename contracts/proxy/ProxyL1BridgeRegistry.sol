ab3e39bdedd1ac7bebac4f9cfe9736bf58b0f108
Zena-park <zena@tokamak.network>
2025-03-01 19:11:51 +0900

    [PLB-01S] Inexistent Event Emission #231

--- contracts/proxy/ProxyL1BridgeRegistry.sol
+++ contracts/proxy/ProxyL1BridgeRegistry.sol
@@ -11,7 +11,7 @@ import "../interfaces/IProxyAction.sol";
 contract ProxyL1BridgeRegistry is ProxyStorage, AuthControlL1BridgeRegistry, IProxyEvent, IProxyAction
 {
 
-    /* ========== DEPENDENCIES ========== */
+    event SetProxyPause(bool _pause);
 
     /* ========== CONSTRUCTOR ========== */
 
@@ -27,6 +27,8 @@ contract ProxyL1BridgeRegistry is ProxyStorage, AuthControlL1BridgeRegistry, IPr
     /// @inheritdoc IProxyAction
     function setProxyPause(bool _pause) external override onlyOwner {
         pauseProxy = _pause;
+
+        emit SetProxyPause(_pause);
     }
 
     /// @notice Set implementation contract
