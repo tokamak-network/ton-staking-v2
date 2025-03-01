3946f9fab3777934ad7d4792f4ba74f8accf6f10
Zena-park <zena@tokamak.network>
2025-03-01 13:51:43 +0900

    [LBR-01C] Inefficient mapping Lookups #186

--- contracts/layer2/L1BridgeRegistryV1_1.sol
+++ contracts/layer2/L1BridgeRegistryV1_1.sol
@@ -188,10 +188,14 @@ contract L1BridgeRegistryV1_1 is ProxyStorage, AuthControlL1BridgeRegistry, L1Br
     )  external onlySeigniorageCommittee() {
         _nonRejected(rollupConfig);
 
-        require (rollupInfo[rollupConfig].rollupType != 0, "NonRegistered");
+        ROLLUP_INFO memory info = rollupInfo[rollupConfig];
 
-        rollupInfo[rollupConfig].rejectedSeigs = true;
-        rollupInfo[rollupConfig].rejectedL2Deposit = true;
+        require (info.rollupType != 0, "NonRegistered");
+
+        info.rejectedSeigs = true;
+        info.rejectedL2Deposit = true;
+
+        rollupInfo[rollupConfig] = info;
 
         ILayer2Manager(layer2Manager).pauseCandidateAddOn(rollupConfig);
         emit RejectedCandidateAddOn(rollupConfig);
@@ -208,8 +212,10 @@ contract L1BridgeRegistryV1_1 is ProxyStorage, AuthControlL1BridgeRegistry, L1Br
     )  external onlySeigniorageCommittee{
         _onlyRejectedRollupConfig(rollupConfig);
 
-        rollupInfo[rollupConfig].rejectedSeigs = false;
-        rollupInfo[rollupConfig].rejectedL2Deposit = rejectedL2Deposit;
+        ROLLUP_INFO memory info = rollupInfo[rollupConfig];
+        info.rejectedSeigs = false;
+        info.rejectedL2Deposit = rejectedL2Deposit;
+        rollupInfo[rollupConfig] = info;
 
         ILayer2Manager(layer2Manager).unpauseCandidateAddOn(rollupConfig);
         emit RestoredCandidateAddOn(rollupConfig);
