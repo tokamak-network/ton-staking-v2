0038fd0cbfa1585379f458d73eef1d822d884b27
HyukSang <zzooppii@gmail.com>
2025-03-06 12:36:03 +0900

    TS-14 - Duplicate code (StorageStateCommittee)
    Deletes AgendaStatus and AgendaResult types defined in the StorageStateCommittee contract.

--- contracts/dao/StorageStateCommittee.sol
+++ contracts/dao/StorageStateCommittee.sol
@@ -10,8 +10,6 @@ import { IDAOVault } from "./interfaces/IDAOVault.sol";
 import { ICandidate } from "./interfaces/ICandidate.sol";
 
 contract StorageStateCommittee  {
-    enum AgendaStatus { NONE, NOTICE, VOTING, EXEC, ENDED, PENDING, RISK }
-    enum AgendaResult { UNDEFINED, ACCEPT, REJECT, DISMISS }
     struct CandidateInfo {
         address candidateContract;
         uint256 indexMembers;
