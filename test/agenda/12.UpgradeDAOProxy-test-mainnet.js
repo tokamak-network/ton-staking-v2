18ba3379d8aa0206c41cd5f7e3e4e7e352ae0d08
HyukSang <zzooppii@gmail.com>
2025-03-07 11:13:34 +0900

    paa the test

--- test/agenda/12.UpgradeDAOProxy-test-mainnet.js
+++ test/agenda/12.UpgradeDAOProxy-test-mainnet.js
@@ -1540,7 +1540,8 @@ describe("DAO Proxy Change Test", () => {
 
         it("27. getOldCandidateInfos (view)", async () => {
             let oldinfo = await daoCommittee_V1_Contract.getOldCandidateInfos(member2Addr)
-            expect(oldinfo.claimedTimestamp).to.be.equal(0)
+            // console.log(oldinfo);
+            expect(oldinfo.rewardPeriod).to.be.equal(0)
         })
 
         it("28. operatorAmountCheck (view)", async () => {
