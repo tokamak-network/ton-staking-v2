78350d7926c763d0ea4544a8beb4265022927270
Zena-park <zena@tokamak.network>
2025-03-11 20:30:19 +0900

    [SM2-04M] Improper Management of Pause States #148

--- contracts/stake/managers/SeigManagerV1_2.sol
+++ contracts/stake/managers/SeigManagerV1_2.sol
@@ -158,6 +158,7 @@ contract SeigManagerV1_2 is ProxyStorage, AuthControlSeigManager, SeigManagerSto
   //////////////////////////////
 
   function pause() public onlyPauser whenNotPaused {
+    revert("Moved to SeigManagerV1_3.");
     _pausedBlock = block.number;
     paused = true;
     emit Paused(msg.sender);
