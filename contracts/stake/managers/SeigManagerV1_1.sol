ba4fe40bdf8729e6f923995de13447504a0f5e9a
Zena-park <zena@tokamak.network>
2025-03-12 23:14:36 +0900

    [SMV-01C] Inexistent Error Message #207

--- contracts/stake/managers/SeigManagerV1_1.sol
+++ contracts/stake/managers/SeigManagerV1_1.sol
@@ -705,7 +705,7 @@ contract SeigManagerV1_1 is ProxyStorage, AuthControlSeigManager, SeigManagerSto
 
 
   function _calcNumSeigBlocks() internal view returns (uint256) {
-    require(!paused);
+    require(!paused, "paused");
 
     uint256 span = block.number - _lastSeigBlock;
     if (_unpausedBlock < _lastSeigBlock) {
