857eed17391256322f05f302e0f40d6ac5d2e029
Zena-park <zena@tokamak.network>
2025-03-12 10:48:52 +0900

    [ACB-01C] Inefficient Application of Modifiers #167 #168

--- contracts/common/AuthControlL1BridgeRegistry.sol
+++ contracts/common/AuthControlL1BridgeRegistry.sol
@@ -25,28 +25,34 @@ contract AuthControlL1BridgeRegistry is AuthRoleL1BridgeRegistry, ERC165Storage,
     /// @dev add admin
     /// @param account  address to add
     function addAdmin(address account) public virtual onlyOwner {
-        grantRole(DEFAULT_ADMIN_ROLE, account);
+        require(!hasRole(DEFAULT_ADMIN_ROLE, account), "already granted");
+        _grantRole(DEFAULT_ADMIN_ROLE, account);
     }
 
     function addManager(address account) public virtual onlyOwner {
-        grantRole(MANAGER_ROLE, account);
+        require(!hasRole(MANAGER_ROLE, account), "already granted");
+        _grantRole(MANAGER_ROLE, account);
     }
 
     function addRegistrant(address account) public virtual onlyManager {
+        require(!hasRole(REGISTRANT_ROLE, account), "already granted");
         _grantRole(REGISTRANT_ROLE, account);
     }
 
     /// @dev remove admin
     /// @param account  address to remove
     function removeAdmin(address account) public virtual onlyOwner {
-        revokeRole(DEFAULT_ADMIN_ROLE, account);
+        require(hasRole(DEFAULT_ADMIN_ROLE, account), "already not granted");
+        _revokeRole(DEFAULT_ADMIN_ROLE, account);
     }
 
     function removeManager(address account) public virtual onlyOwner {
-        revokeRole(MANAGER_ROLE, account);
+        require(hasRole(MANAGER_ROLE, account), "already not granted");
+        _revokeRole(MANAGER_ROLE, account);
     }
 
     function removeRegistrant(address account) public virtual onlyManager {
+        require(hasRole(REGISTRANT_ROLE, account), "already not granted");
         _revokeRole(REGISTRANT_ROLE, account);
     }
 
@@ -55,29 +61,34 @@ contract AuthControlL1BridgeRegistry is AuthRoleL1BridgeRegistry, ERC165Storage,
     function transferAdmin(address newAdmin) public virtual onlyOwner {
         require(newAdmin != address(0), "Accessible: zero address");
         require(msg.sender != newAdmin, "Accessible: same admin");
+        require(!hasRole(DEFAULT_ADMIN_ROLE, newAdmin), "already granted");
 
-        grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
-        renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+        _revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function renounceOwnership() public {
-        renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    function renounceOwnership() public onlyOwner {
+        _revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function renounceManager() public {
-        renounceRole(MANAGER_ROLE, msg.sender);
+        require(hasRole(MANAGER_ROLE, msg.sender), "already not granted");
+        _revokeRole(MANAGER_ROLE, msg.sender);
     }
 
     function renounceRegistrant() public {
-        renounceRole(REGISTRANT_ROLE, msg.sender);
+        require(hasRole(REGISTRANT_ROLE, msg.sender), "already not granted");
+        _revokeRole(REGISTRANT_ROLE, msg.sender);
     }
 
     function revokeManager(address account) public onlyOwner {
-        revokeRole(MANAGER_ROLE, account);
+        require(hasRole(MANAGER_ROLE, account), "already not granted");
+        _revokeRole(MANAGER_ROLE, account);
     }
 
     function revokeRegistrant(address account) public onlyOwner {
-        revokeRole(REGISTRANT_ROLE, account);
+        require(hasRole(REGISTRANT_ROLE, account), "already not granted");
+        _revokeRole(REGISTRANT_ROLE, account);
     }
 
     /// @dev whether admin
