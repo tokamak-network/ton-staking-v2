7418aa6bd8901a86262eeb5deaece44095fba4ed
Zena-park <zena@tokamak.network>
2025-03-13 09:57:08 +0900

    [DMW-01M] Potential of Data Corruption #116

--- contracts/stake/managers/DepositManager_setWithdrawalDelay.sol
+++ contracts/stake/managers/DepositManager_setWithdrawalDelay.sol
@@ -47,7 +47,7 @@ contract DepositManager_setWithdrawalDelay is ProxyStorage, AccessibleCommon, De
      * @param withdrawalDelay_      The number of withdrawal delay blocks
     */
     function setWithdrawalDelayByOwner(address layer2, uint256 withdrawalDelay_) external onlyOwner {
-        require(globalWithdrawalDelay < withdrawalDelay_, "wrong withdrawalDelay");
+        require(withdrawalDelay_ > globalWithdrawalDelay && withdrawalDelay_ <= MAX_DELAY_BLOCKS, "Not acceptable");
         withdrawalDelay[layer2] = withdrawalDelay_;
         emit SetWithdrawalDelayByOwner(layer2, withdrawalDelay_);
     }
