3b60939c193387198b3594634c25dd9ef1e3aff3
Zena-park <zena@tokamak.network>
2025-03-14 11:07:20 +0900

    [ARB-01C] Repetitive Authorization Role Declaration #174

--- contracts/common/AuthRoleL1BridgeRegistry.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-contract AuthRoleL1BridgeRegistry {
-    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER");
-    bytes32 public constant REGISTRANT_ROLE = keccak256("REGISTRANT");
-}
--- contracts/common/AuthRoleLayer2Manager.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-contract AuthRoleLayer2Manager {
-    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER");
-    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR");
-}
