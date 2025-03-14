af4e8008ff6d84335e21f6af04e8e0cf93117e4b
Zena-park <zena@tokamak.network>
2025-03-14 13:03:19 +0900

    [PCE-01S] Inexistent Event Emission #230

--- contracts/proxy/ProxyCoinage.sol
+++ contracts/proxy/ProxyCoinage.sol
@@ -26,6 +26,7 @@ contract ProxyCoinage is ProxyStorage, AuthControlCoinage, IProxyEvent, IProxyAc
 
     /// @inheritdoc IProxyAction
     function setProxyPause(bool _pause) external override onlyOwner {
+        require(pauseProxy != _pause, "same");
         pauseProxy = _pause;
 
         emit SetProxyPause(_pause);
