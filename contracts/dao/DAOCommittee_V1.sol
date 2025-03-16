19830e719c3ffed08858a935ccfb38f0aba858fb
HyukSang <zzooppii@gmail.com>
2025-03-16 17:45:26 +0900

    [DAO-01M] Inexistent Imposition of Access Control #264
    You can view the results through the veiw function without changing the agenda results through endAgendaVoting.

--- contracts/dao/DAOCommittee_V1.sol
+++ contracts/dao/DAOCommittee_V1.sol
@@ -506,8 +506,35 @@ contract DAOCommittee_V1 is
 
     /// @notice Set the agenda status as ended(denied or dismissed)
     /// @param _agendaID Agenda ID
-    function endAgendaVoting(uint256 _agendaID) external {
-        agendaManager.endAgendaVoting(_agendaID);
+    function endAgendaVoting(uint256 _agendaID) external view returns (uint256 agendaResult, uint256 agendaStatus) {
+        // agendaManager.endAgendaVoting(_agendaID);
+        //Result -> 0: pending, 1: ACCEPT, 2: REJECT, 3: DISMISS
+        //Status -> 0: NONE, 1: NOTICE, 2: VOTING, 3: WAITING_EXEC, 4: EXECUTED, 5: ENDED
+        uint256 voingEndTime = agendaManager.getAgendaVotingEndTimeSeconds(_agendaID);
+        require(block.timestamp > voingEndTime, "need over vote");
+        (uint256 yes, uint256 no,) = agendaManager.getVotingCount(_agendaID);
+        if (quorum <= yes) {
+            // yes
+            (uint256 result, bool executed) = agendaManager.getAgendaResult(_agendaID);
+            agendaResult = result;
+            if (executed) {
+                agendaStatus = 4;
+            } else {
+                agendaStatus = 3;
+            }
+            return (agendaResult, agendaStatus);
+        } else if (quorum <= no) {
+            // no (REJECT, ENDED)
+            agendaResult = 2;
+            agendaStatus = 5;
+            return (agendaResult, agendaStatus);
+        } else {
+            // (DISMISS, ENDED)
+            agendaResult = 3;
+            agendaStatus = 5;
+            return (agendaResult, agendaStatus);
+        }
+
     }
 
     /// @notice Execute the accepted agenda
