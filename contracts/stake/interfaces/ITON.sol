5c58c855ec45adc708f8c088a904c0c947e3ec3a
Zena-park <zena@tokamak.network>
2025-03-30 13:11:20 +0900

    Multiple Top-Level Declarations #183

--- contracts/stake/interfaces/ITON.sol
+++ contracts/stake/interfaces/ITON.sol
@@ -5,4 +5,7 @@ interface ITON {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function approveAndCall(address spender, uint256 amount, bytes memory data) external returns (bool);
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+
 }
