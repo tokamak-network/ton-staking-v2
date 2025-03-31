52e63c5e0504a467c92105ea025ab4f401ebd3f6
HyukSang <zzooppii@gmail.com>
2025-03-31 15:30:56 +0900

    update check Owner

---
+++ test/agenda/22.checkDAOOwner-test-mainnet.js
@@ -0,0 +1,456 @@
+const hre = require("hardhat");
+const { ethers } = hre;
+const fs = require('fs');
+const { readContracts, deployedContracts } = require("../common_func");
+const chai = require("chai");
+const { solidity } = require("ethereum-waffle");
+
+const { expect, assert } = chai;
+chai.use(solidity);
+require("chai").should();
+
+const Web3EthAbi = require('web3-eth-abi');
+const { padLeft } = require('web3-utils');
+
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+
+const networkName = "mainnet"
+
+const daoAdminAddress = '0xb4983da083a5118c903910db4f5a480b1d9f3687'
+// const daoAdminAddress = '0x757DE9c340c556b56f62eFaE859Da5e08BAAE7A2'
+
+const goerliPowerTonAdmin = "0xc1eba383D94c6021160042491A5dfaF1d82694E6"
+const mainnetPowerTonAdmin = "0x15280a52e79fd4ab35f4b9acbb376dcd72b44fd1"
+
+let daoAgendaMangerAddress = "0xcD4421d082752f363E1687544a09d5112cD4f484"; //DAOAgendaManager Address
+
+const TonABI = require("../../abi/TON.json").abi;
+const WtonABI = require("../../abi/WTON.json").abi;
+const DAOCommitteeExtendABI = require("../../abi/DAOCommitteeExtend.json").abi;
+const DAOCommitteeOwnerABI = require("../../artifacts/contracts/dao/DAOCommitteeOwner.sol/DAOCommitteeOwner.json").abi;
+const DAOCommitteeDAOVaultABI = require("../../artifacts/contracts/dao/DAOCommitteeDAOVault.sol/DAOCommitteeDAOVault.json").abi;
+const DAOCommittee_V1ABI = require("../../artifacts/contracts/dao/DAOCommittee_V1.sol/DAOCommittee_V1.json").abi;
+const DAOCommitteeProxyABI = require("../../abi/DAOCommitteeProxy.json").abi;
+const DAOProxy2ABI = require("../../artifacts/contracts/proxy/DAOCommitteeProxy2.sol/DAOCommitteeProxy2.json").abi;
+const SeigManagerProxyABI = require("../../artifacts/contracts/stake/managers/SeigManagerProxy.sol/SeigManagerProxy.json").abi;
+const SeigManagerABI = require("../../artifacts/contracts/stake/managers/SeigManager.sol/SeigManager.json").abi;
+const SeigManagerV1ABI = require("../../artifacts/contracts/stake/managers/SeigManagerV1_1.sol/SeigManagerV1_1.json").abi;
+const DepositManagerABI = require("../../artifacts/contracts/stake/managers/DepositManager.sol/DepositManager.json").abi;
+
+const DAOAgendaManagerABI = require("../../abi/daoAgendaManager.json").abi;
+const DAOVaultABI = require("../../abi/DAOVault.json").abi;
+
+const CandidateABI = require("../../abi/Candidate.json").abi;
+const DepositManagerProxy_Json = require('../../abi/DepositManagerProxy.json')
+const SeigManagerProxy_Json = require('../../abi/DepositManagerProxy.json')
+
+
+describe("DAO Proxy Change Test", () => {
+
+    let daoCommitteeAdmin;
+    let daoCommitteeAdminContract;
+    let daoCommitteeProxy;
+    // let daoCommitteeDAOVaultLogic;
+    let daoCommitteeOwnerLogic;
+    let ton;
+    let wton;
+    
+    let depositManagerContract;
+    let seigManagerContract;
+    let seigManagerProxy;
+    let seigManagerProxyContract;
+    let seigManagerV1Contract;
+
+    let daoCommittee;
+    let daoCommitteeOwner;
+    let daoagendaManager;
+    let daovault;
+
+    let seigManagerV1_3;
+    let depositManagerV1_1;
+
+    let testAddr = "f0B595d10a92A5a9BC3fFeA7e79f5d266b6035Ea";
+    let tonAddr = "2be5e8c109e2197D077D13A82dAead6a9b3433C5";
+    let wtonAddr = "c4A11aaf6ea915Ed7Ac194161d2fC9384F15bff2";
+
+    let pause_role = "0xfcb9fcbfa83b897fb2d5cf4b58962164105c1e71489a37ef3ae0db3fdce576f6"
+
+    let member1;
+    let member2;
+    let member3;
+
+    let newMember1;
+
+    let talken;
+
+    let member1Contract;
+    let member2Contract;
+    let member3Contract;
+    let talkenContract;
+
+    let member1ContractLogic;
+    let member2ContractLogic;
+    let member3ContractLogic;
+    let newMember1ContractLogic;
+    let talkenContractLogic;
+
+    let newMember1Contract;
+
+    let member1Addr = "0x39a13a796a3cd9f480c28259230d2ef0a7026033"
+    let member2Addr = "0xd1820b18be7f6429f1f44104e4e15d16fb199a43"
+    let member2AddrUpper = "0xD1820b18bE7f6429F1f44104e4E15d16Fb199a43"
+    let member3Addr = "0x42adfaae7db56b294225ddcfebef48b337b34b23"
+
+    let member4Addr = "0xbc8896ebb2e3939b1849298ef8da59e09946cf66"
+
+    let newMember1Addr = "0xea8e2ec08dcf4971bdcdfffe21439995378b44f3"
+    let newMember1ContractAddr = "0xf3B17FDB808c7d0Df9ACd24dA34700ce069007DF"
+
+    let member1ContractAddr = "0x576c7a48fcef1c70db632bb1504d9a5c0d0190d3"
+    // let member2ContractAddr = "0x42ccf0769e87cb2952634f607df1c7d62e0bbc52"
+    let member2ContractAddr = "0x0F42D1C40b95DF7A1478639918fc358B4aF5298D"
+    // let member3ContractAddr = "0x5d9a0646c46245a8a3b4775afb3c54d07bcb1764"
+    let member3ContractAddr = "0x06D34f65869Ec94B3BA8c0E08BCEb532f65005E2"
+
+    let talkenAddr = "0xcc2f386adca481a00d614d5aa77a30984f264a07"
+    let talkenUpperAddr = "0xCC2f386adcA481a00d614d5AA77A30984F264A07"
+    let talkenContractAddr = "0x36101b31e74c5E8f9a9cec378407Bbb776287761"
+
+    let beforeclaimAmount;
+    let afterclaimAmount;
+
+    let wtonCheck = "0xf0B595d10a92A5a9BC3fFeA7e79f5d266b6035Ea";
+
+    let sendether = "0xDE0B6B3A7640000"
+
+    let zeroAddr = "0x0000000000000000000000000000000000000000";
+    let oneAddr = "0x0000000000000000000000000000000000000001";
+    let twoAddr = "0x0000000000000000000000000000000000000002";
+    let tosAddr = "0x409c4D8cd5d2924b9bc5509230d16a61289c8153";
+
+    let adminBytes = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+
+    // mainnet network
+    const oldContractInfo = {
+        TON: "0x2be5e8c109e2197D077D13A82dAead6a9b3433C5",
+        WTON: "0xc4A11aaf6ea915Ed7Ac194161d2fC9384F15bff2",
+        Layer2Registry: "0x0b3E174A2170083e770D5d4Cf56774D221b7063e",
+        DepositManager: "0x56E465f654393fa48f007Ed7346105c7195CEe43",
+        CoinageFactory: "0x5b40841eeCfB429452AB25216Afc1e1650C07747",
+        OldDAOVaultMock: "",
+        SeigManager: "0x710936500aC59e8551331871Cbad3D33d5e0D909",
+        PowerTON: "0x970298189050aBd4dc4F119ccae14ee145ad9371",
+        DAOVault: "0x2520CD65BAa2cEEe9E6Ad6EBD3F45490C42dd303",
+        DAOAgendaManager: "0xcD4421d082752f363E1687544a09d5112cD4f484",
+        CandidateFactory: "0xE6713aF11aDB0cFD3C60e15b23E43f5548C32942",
+        DAOCommittee: "0xd1A3fDDCCD09ceBcFCc7845dDba666B7B8e6D1fb",
+        DAOCommitteeProxy: "0xDD9f0cCc044B0781289Ee318e5971b0139602C26"
+    }
+
+    const nowContractInfo = {
+        TON: "0x2be5e8c109e2197D077D13A82dAead6a9b3433C5",
+        WTON: "0xc4A11aaf6ea915Ed7Ac194161d2fC9384F15bff2",
+        Layer2Registry: "0x7846c2248a7b4de77e9c2bae7fbb93bfc286837b",
+        DepositManager: "0x0b58ca72b12f01fc05f8f252e226f3e2089bd00e",
+        CoinageFactory: "",
+        SeigManager: "0x0b55a0f463b6defb81c6063973763951712d0e5f",
+    }
+
+    let SeigManagerUpper = "0x0b55a0f463b6DEFb81c6063973763951712D0E5F"
+
+    let minimumAmount = ethers.utils.parseUnits("1000", 18);
+
+    let daoCommitteeDAOVaultLogic = "0xba5634e0c432af80060cf19e0940b59b2dc31173"
+
+    let daoCommitteeLogic;
+
+    let daoCommittee_V1_Contract;
+    let daoCommittee_Owner_Contract;
+
+    let daoCommitteeProxy2;
+    let daoCommitteeProxy2Contract;
+
+    let user1;
+    let user1Addr = "0x9FC3da866e7DF3a1c57adE1a97c9f00a70f010c8"
+    let user1Contract;
+    let user1ContractLogic;
+    let user1ContractAddr;
+
+    let user2;
+    let user2Addr = "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97"
+    let user2Contract;
+    let user2ContractLogic;
+    let user2ContractAddr;
+
+    let agendaID;
+    let beforeAgendaID;
+
+    let l1BridgeRegistryV_1;
+    let l1BridgeRegistryProxy;
+    let l1BridgeRegistry;
+    let layer2ManagerV1_1;
+    let layer2ManagerProxy;
+    let candidateAddOnFactoryImp;
+    let candidateAddOnFactoryProxy;
+    let legacySystemConfig;
+
+    //changeMember before info
+    // [
+    //     '0x576C7a48fcEf1C70db632bb1504D9A5C0D0190D3',
+    //     BigNumber { value: "0" },
+    //     BigNumber { value: "1615364700" },
+    //     BigNumber { value: "0" },
+    //     BigNumber { value: "1635223931" },
+    //     candidateContract: '0x576C7a48fcEf1C70db632bb1504D9A5C0D0190D3',
+    //     indexMembers: BigNumber { value: "0" },
+    //     memberJoinedTime: BigNumber { value: "1615364700" },
+    //     rewardPeriod: BigNumber { value: "0" },
+    //     claimedTimestamp: BigNumber { value: "1635223931" }
+    //   ]
+
+    before('create fixture loader', async () => {
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            daoAdminAddress,
+        ]);
+        daoCommitteeAdmin = await hre.ethers.getSigner(daoAdminAddress);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            oldContractInfo.DAOCommitteeProxy,
+        ]);
+        daoCommitteeAdminContract =  await hre.ethers.getSigner(oldContractInfo.DAOCommitteeProxy);
+        
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            member1Addr,
+        ]);
+        member1 = await hre.ethers.getSigner(member1Addr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            newMember1Addr,
+        ]);
+        newMember1 = await hre.ethers.getSigner(newMember1Addr);
+        
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            member2Addr,
+        ]);
+        member2 = await hre.ethers.getSigner(member2Addr);
+        
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            member3Addr,
+        ]);
+        member3 = await hre.ethers.getSigner(member3Addr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            talkenAddr,
+        ]);
+        talken = await hre.ethers.getSigner(talkenAddr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            member1ContractAddr,
+        ]);
+        member1Contract = await hre.ethers.getSigner(member1ContractAddr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            newMember1ContractAddr,
+        ]);
+        newMember1Contract = await hre.ethers.getSigner(newMember1ContractAddr);
+        
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            member2ContractAddr,
+        ]);
+        member2Contract = await hre.ethers.getSigner(member2ContractAddr);
+        
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            member3ContractAddr,
+        ]);
+        member3Contract = await hre.ethers.getSigner(member3ContractAddr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            talkenContractAddr,
+        ]);
+        talkenContract = await hre.ethers.getSigner(talkenContractAddr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            user1Addr,
+        ]);
+        user1 = await hre.ethers.getSigner(user1Addr);
+
+        await hre.network.provider.send("hardhat_impersonateAccount", [
+            user2Addr,
+        ]);
+        user2 = await hre.ethers.getSigner(user2Addr);
+        
+        await hre.network.provider.send("hardhat_setBalance", [
+            member1ContractAddr,
+            sendether
+        ]);
+
+        await hre.network.provider.send("hardhat_setBalance", [
+            member2ContractAddr,
+            sendether
+        ]);
+
+        await hre.network.provider.send("hardhat_setBalance", [
+            member3ContractAddr,
+            sendether
+        ]);
+
+        await hre.network.provider.send("hardhat_setBalance", [
+            newMember1ContractAddr,
+            sendether
+        ]);
+
+        await hre.network.provider.send("hardhat_setBalance", [
+            member3Addr,
+            sendether
+        ]);
+
+        await hre.network.provider.send("hardhat_setBalance", [
+            talkenAddr,
+            sendether
+        ]);
+
+        await hre.network.provider.send("hardhat_setBalance", [
+            daoCommitteeAdminContract.address,
+            sendether
+        ]);
+    })
+
+    describe("Setting TON-related Contract", () => {
+        it("Set TON", async () => {
+            ton = new ethers.Contract(
+                oldContractInfo.TON,
+                TonABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("Set WTON", async () => {
+            wton = new ethers.Contract(
+                oldContractInfo.WTON,
+                WtonABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("Set SeigManager", async () => {
+            seigManagerContract = new ethers.Contract(
+                nowContractInfo.SeigManager,
+                SeigManagerABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("TON Admin Test", async () => {
+            let balanceOfZero = await ton.balanceOf(ethers.constants.AddressZero)
+            let balanceOfdaoAdminAddress = await ton.balanceOf(daoAdminAddress)
+            // console.log('balanceOfZero' , balanceOfZero)
+            // console.log('balanceOfdaoAdminAddress' , balanceOfdaoAdminAddress)
+
+            await (await ton.connect(daoCommitteeAdmin).transfer(
+                '0x0000000000000000000000000000000000000001',
+                ethers.BigNumber.from("1")
+            )).wait()
+
+            balanceOfdaoAdminAddress = await ton.balanceOf(daoAdminAddress)
+            // console.log('balanceOfdaoAdminAddress' , balanceOfdaoAdminAddress)
+        })
+    })
+
+    describe("Set Contract", () => {
+        it("Set DAOProxy", async () => {
+            daoCommitteeProxy = new ethers.Contract(
+                oldContractInfo.DAOCommitteeProxy,
+                DAOCommitteeProxyABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("set DAOProxy2", async () => {
+            daoCommitteeProxy2Contract = new ethers.Contract(
+                oldContractInfo.DAOCommitteeProxy,
+                DAOProxy2ABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("pauseProxy check", async () => {
+            let pauseProxy = await daoCommitteeProxy.pauseProxy()
+            // console.log('pauseProxy', pauseProxy)
+
+            if (pauseProxy == true) {
+                await (await daoCommitteeProxy.connect(daoCommitteeAdmin).setProxyPause(false)).wait()
+            }
+            pauseProxy = await daoCommitteeProxy.pauseProxy()
+            // console.log('pauseProxy', pauseProxy)
+        })
+
+        it("set DAOAgendaManager", async () => {
+            daoagendaManager = new ethers.Contract(
+                oldContractInfo.DAOAgendaManager,
+                DAOAgendaManagerABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("set DAO NewLogic", async () => {
+            daoCommittee_V1_Contract = new ethers.Contract(
+                daoCommitteeProxy.address,
+                DAOCommittee_V1ABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        it("set DAO OnwerLogic", async () => {
+            daoCommittee_Owner_Contract = new ethers.Contract(
+                daoCommitteeProxy.address,
+                DAOCommitteeOwnerABI,
+                daoCommitteeAdmin
+            )
+        })
+
+        // it("set depositManager", async () => {
+        //     depositManagerProxy = new ethers.Contract(
+        //         nowContractInfo.DepositManager,  
+        //         DepositManagerProxy_Json.abi, 
+        //         daoCommitteeAdmin
+        //     )
+        // })
+
+        // it("set seigManagerProxy", async () => {
+        //     seigManagerProxy = new ethers.Contract(
+        //         nowContractInfo.SeigManager,  
+        //         SeigManagerProxy_Json.abi, 
+        //         daoCommitteeAdmin
+        //     )
+        // })
+
+        // it("set l1BridgeRegistry", async () => {
+        //     l1BridgeRegistry = await ethers.getContractAt(
+        //         "L1BridgeRegistryV1_1", 
+        //         l1BridgeRegistryProxy.address, 
+        //         daoCommitteeAdmin
+        //     )
+        // })
+
+    })
+
+    describe("DAOCommitteeProxy", () => {
+        // it("check delete account Owner", async () => {
+        //     let adminRole = "0x0000000000000000000000000000000000000000000000000000000000000000"
+        //     let address = ""
+        //     let checkOwner = await daoCommitteeProxy.hasRole(adminRole,address)
+        //     // console.log(checkOwner)
+        //     expect(checkOwner).to.be.equal(false)
+        // })
+
+        it("check MultiSigWallet has DAOOwner", async () => {
+            let adminRole = "0x0000000000000000000000000000000000000000000000000000000000000000"
+            let address = ""
+            let checkOwner = await daoCommitteeProxy.hasRole(adminRole,address)
+            // console.log(checkOwner)
+            expect(checkOwner).to.be.equal(true)
+        })
+    })
+
+})
