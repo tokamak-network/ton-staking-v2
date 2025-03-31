d4eba643d941b9a766d0829646c2d85805ba33b0
Zena-park <zena@tokamak.network>
2025-03-31 17:19:18 +0900

    Update SeigManagerV1_3.sol

--- contracts/stake/managers/SeigManagerV1_3.sol
+++ contracts/stake/managers/SeigManagerV1_3.sol
@@ -699,39 +699,5 @@ contract SeigManagerV1_3 is
             burntAmount;
     }
 
-    function test_SetNumber() public {
-        uint256 span = 1;
-        uint256 tos = 1_000_000 * 1e27;
-        uint256 totalLayer2TVL = 1e27 - 1;
-        uint256 maxSeig = span * _seigPerBlock;
-        uint256 prevTotalSupply = tos - totalLayer2TVL;
-        console.log("maxSeig %s", maxSeig);
-
-        uint256 stakedSeig = FullMath.rdiv(
-            FullMath.rmul(maxSeig, prevTotalSupply),
-            tos
-        );
-        console.log("stakedSeig %s", stakedSeig);
-
-        uint256 _totalLayer2TVL = Math.min(
-            totalLayer2TVL,
-            tos - prevTotalSupply
-        );
-        if (_totalLayer2TVL < 1e27) _totalLayer2TVL = 0;
-
-        console.log("_totalLayer2TVL %s", _totalLayer2TVL);
-
-        uint256 l2TotalSeigs_1 =  FullMath.rmul(maxSeig, _totalLayer2TVL);
-        console.log("l2TotalSeigs_1 %s", l2TotalSeigs_1);
-
-        uint256 l2TotalSeigs = FullMath.rdiv(
-            l2TotalSeigs_1,
-            tos
-        );
-
-        console.log("l2TotalSeigs %s", l2TotalSeigs);
-        uint256 unstakedSeig = maxSeig - stakedSeig - l2TotalSeigs;
-        console.log("unstakedSeig %s", unstakedSeig);
-    }
 
 }
