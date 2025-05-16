a4eb49796ff9d6d805994a83f3c6e8455733779e
HyukSang <zzooppii@gmail.com>
2025-05-16 16:32:21 +0900

    logic Update

--- contracts/dao/DAOCommittee_V1.sol
+++ contracts/dao/DAOCommittee_V1.sol
@@ -21,6 +21,8 @@ import "./StorageStateCommitteeV2.sol";
 import "./lib/BytesLib.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+import "hardhat/console.sol";
+
 /**
  * @notice Error that occurs when creating Candidate
  * @param x 1: deployed candidateContract is zero
@@ -517,15 +519,58 @@ contract DAOCommittee_V1 is
     function currentAgendaStatus(uint256 _agendaID) external view returns (uint256 agendaResult, uint256 agendaStatus) {
         //Result -> 0: pending, 1: ACCEPT, 2: REJECT, 3: DISMISS, 4: NO CONSENSUS, 5: NO AGENDA
         //Status -> 0: NONE, 1: NOTICE, 2: VOTING, 3: WAITING_EXEC, 4: EXECUTED, 5: ENDED, 6: NO AGENDA
-        uint256 noticeEndTime = agendaManager.getAgendaNoticeEndTimeSeconds(_agendaID);
-        uint256 votingEndTime = agendaManager.getAgendaVotingEndTimeSeconds(_agendaID);
-        if(noticeEndTime == 0) {
+        uint256 numAgendas = agendaManager.numAgendas();
+        if(numAgendas <=  _agendaID){
             // No Agenda
             return (5, 6);
-        } else if (block.timestamp < noticeEndTime) {
+        }
+
+        uint256 noticeEndTime = agendaManager.getAgendaNoticeEndTimeSeconds(_agendaID);
+        uint256 votingEndTime = agendaManager.getAgendaVotingEndTimeSeconds(_agendaID);
+        // (uint256 yes, uint256 no, uint256 abstain) = agendaManager.getVotingCount(_agendaID);
+        // uint256 totalvotes = yes + no + abstain;
+        
+        if (block.timestamp < noticeEndTime) {
             //Notice Time
+            //(PENDING, NOTICE)
             return (0, 1);
-        } else if (noticeEndTime < block.timestamp) {
+        } else if (noticeEndTime <= block.timestamp && votingEndTime == 0) {
+            //NoticeTime은 지났지만 아무도 투표 안했을때
+            //(NO CONSENSUS, VOTING)
+            agendaResult = 4;
+            agendaStatus = 2;
+            return (agendaResult, agendaStatus);
+        } else if (noticeEndTime <= block.timestamp &&  block.timestamp <= votingEndTime) {
+            //NoticeTime이 지나고 누군가 투표 하였고 투표가 종료되지 않았을때
+            console.log("1");
+            // (uint256 yes, uint256 no, uint256 abstain) = agendaManager.getVotingCount(_agendaID);
+            (uint256 result,) = agendaManager.getAgendaResult(_agendaID);
+            agendaStatus = 2;
+            return (result, agendaStatus);
+            // if (quorum <= yes) {
+            //     // yes (ACCPECT, VOTING)
+            //     agendaResult = 1;
+            //     agendaStatus = 2;
+            //     return (agendaResult, agendaStatus);
+            // } else if (quorum <= no) {
+            //     // no (REJECT, VOTING)
+            //     agendaResult = 2;
+            //     agendaStatus = 2;
+            //     return (agendaResult, agendaStatus);
+            // } else if (quorum <= abstain) {
+            //     // (DISMISS, VOTING)
+            //     agendaResult = 3;
+            //     agendaStatus = 2;
+            //     return (agendaResult, agendaStatus);
+            // } else {
+            //     // (NO CONSENSUS, VOTING)
+            //     agendaResult = 4;
+            //     agendaStatus = 2;
+            //     return (agendaResult, agendaStatus);
+            // }
+        } else if (votingEndTime < block.timestamp && votingEndTime != 0) {
+            //votingEndTime이 지난뒤 결과
+            console.log("2");
             (uint256 yes, uint256 no, uint256 abstain) = agendaManager.getVotingCount(_agendaID);
             if (quorum <= yes) {
                 // yes
