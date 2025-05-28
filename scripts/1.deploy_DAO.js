e7cc3beea6e1cb393638f80929b2794ec17859bb
HyukSang <zzooppii@gmail.com>
2025-05-28 14:24:23 +0900

    add Deploy V2, V3

--- scripts/1.deploy_DAO.js
+++ scripts/1.deploy_DAO.js
@@ -18,17 +18,35 @@ async function DeployDAOCommitteeOwner() {
 }
 
 async function DeployDAOCommittee_V1() {
-    //==== DAOCommitteeOwner =================================
+    //==== DAOCommittee_V1 =================================
     const DAOCommittee_V1Dep = await ethers.getContractFactory("DAOCommittee_V1");
     const DAOCommittee_V1 = await DAOCommittee_V1Dep.deploy();
     await DAOCommittee_V1.deployed();
     console.log('DAOCommittee_V1' , DAOCommittee_V1.address)
 }
 
+async function DeployDAOCommittee_V2() {
+    //==== DAOCommittee_V2 =================================
+    const DAOCommittee_V2Dep = await ethers.getContractFactory("DAOCommittee_V2");
+    const DAOCommittee_V2 = await DAOCommittee_V2Dep.deploy();
+    await DAOCommittee_V2.deployed();
+    console.log('DAOCommittee_V2' , DAOCommittee_V2.address)
+}
+
+async function DeployDAOCommittee_V3() {
+    //==== DAOCommittee_V3 =================================
+    const DAOCommittee_V3Dep = await ethers.getContractFactory("DAOCommittee_V3");
+    const DAOCommittee_V3 = await DAOCommittee_V3Dep.deploy();
+    await DAOCommittee_V3.deployed();
+    console.log('DAOCommittee_V3' , DAOCommittee_V3.address)
+}
+
 const main = async () => {
   // await DeployDAOCommitteeDAOVault()
   // await DeployDAOCommitteeOwner()
-  await DeployDAOCommittee_V1()
+  // await DeployDAOCommittee_V1()
+  // await DeployDAOCommittee_V2()
+  await DeployDAOCommittee_V3()
 }
 
 
