e5f4f781c8e8bf93c54072e65a4de1badf81b1d2
HyukSang <zzooppii@gmail.com>
2025-09-03 16:40:23 +0900

    update protocolKit

--- scripts/safeWallet-protocol/protocolKit.ts
+++ scripts/safeWallet-protocol/protocolKit.ts
@@ -6,12 +6,14 @@
 
 import 'dotenv/config'
 import Safe, {
+    EthSafeSignature,
     PredictedSafeProps,
     SafeAccountConfig,
     buildContractSignature,
     buildSignatureBytes,
     hashSafeMessage
 } from '@safe-global/protocol-kit'
+import { SigningMethod } from '@safe-global/types-kit';
 import SafeApiKit from '@safe-global/api-kit'
 import hre, { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
@@ -23,7 +25,7 @@ const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY;
 const OWNER_PRIVATE_KEY2 = process.env.OWNER_PRIVATE_KEY2;
 const SAFE_ADDRESS = process.env.SAFE_ADDRESS;
 const DAO_ADDRESS = process.env.DAO_ADDRESS;
-// const MULTISIG_ADDRESS = process.env.MULTISIG_ADDRESS;
+const MULTISIG_ADDRESS = process.env.MULTISIG_ADDRESS;
 const SAFE_API_KEY = process.env.SAFE_API_KEY;
 
 let multiSigOwner1: SignerWithAddress;
@@ -77,7 +79,7 @@ async function main(): Promise<void> {
     }
 
     const safeAccountConfig: SafeAccountConfig = {
-        owners: ['0xA2101482b28E3D99ff6ced517bA41EFf4971a386', '0x6E1c4a442E9B9ddA59382ee78058650F1723E0F6', '0x3bFda92Fa3bC0AB080Cac3775147B6318b1C5115'],
+        owners: ['0x80047c450ee203c4A970A7005d6cd30b2F34C98f', '0x6E1c4a442E9B9ddA59382ee78058650F1723E0F6', '0x3bFda92Fa3bC0AB080Cac3775147B6318b1C5115'],
         threshold: 2
         // More optional properties
     }
@@ -93,12 +95,12 @@ async function main(): Promise<void> {
         predictedSafe
     })
 
-    // 2. 메인 Safe 인스턴스 생성 (DAOContract가 소유자인 Safe)
-    const mainSafeKit = await Safe.init({
-        provider: RPC_URL!,
-        signer: OWNER_PRIVATE_KEY!,
-        safeAddress: SAFE_ADDRESS!
-    });
+    // // 2. 메인 Safe 인스턴스 생성 (DAOContract가 소유자인 Safe)
+    // const mainSafeKit = await Safe.init({
+    //     provider: RPC_URL!,
+    //     signer: OWNER_PRIVATE_KEY!,
+    //     safeAddress: SAFE_ADDRESS!
+    // });
 
 
     // 3. Safe 트랜잭션 데이터 생성
@@ -115,25 +117,79 @@ async function main(): Promise<void> {
         "safeTxGas": "0"
     }
 
-    const safeTransaction = await mainSafeKit.createTransaction({
+    // const safeTransaction = await protocolKit.createTransaction({
+    let transactionSafe2_3 = await protocolKit.createTransaction({
         transactions: [safeTransactionData]
     });
 
+    // Connect OWNER_4_ADDRESS(MultiSigWallet의 Owner) and the address of SAFE_2_3_ADDRESS(MultiSigWallet)
+    protocolKit = await protocolKit.connect({
+        provider: RPC_URL!,
+        signer: OWNER_PRIVATE_KEY!,
+        safeAddress: MULTISIG_ADDRESS!
+    })
+
+
+    // Sign the transactionSafe2_3 with OWNER_4_ADDRESS
+    // After this, the transactionSafe2_3 contains the signature from OWNER_4_ADDRESS
+    // Parent Safe Address => SafeWalletAddress
+    transactionSafe2_3 = await protocolKit.signTransaction(
+        transactionSafe2_3,
+        SigningMethod.SAFE_SIGNATURE,
+        SAFE_ADDRESS // Parent Safe address
+    )
+    
+    // Connect OWNER_5_ADDRESS(MultiSigWallet의 Owner2)
+    protocolKit = await protocolKit.connect({
+        provider: RPC_URL,
+        signer: OWNER_PRIVATE_KEY2
+    })
+    
+    // Sign the transactionSafe2_3 with OWNER_5_ADDRESS
+    // After this, the transactionSafe2_3 contains the signature from OWNER_5_ADDRESS
+    transactionSafe2_3 = await protocolKit.signTransaction(
+        transactionSafe2_3,
+        SigningMethod.SAFE_SIGNATURE,
+        SAFE_ADDRESS // Parent Safe address
+    )
+
+    // Build the contract signature of SAFE_2_3_ADDRESS
+    const signatureSafe2_3 = await buildContractSignature(
+        Array.from(transactionSafe2_3.signatures.values()),
+        DAO_ADDRESS!
+    )
+    // console.log("signatureSafe2_3 : ", signatureSafe2_3);
+    
+    // Add the signatureSafe2_3 to safeTransaction
+    // After this, the safeTransaction contains the signature from OWNER_1_ADDRESS, OWNER_2_ADDRESS, SAFE_1_1_ADDRESS and SAFE_2_3_ADDRESS
+    transactionSafe2_3.addSignature(signatureSafe2_3)
+
+    const safeTransactionHash = await protocolKit.getTransactionHash(transactionSafe2_3)
+    
+    const signerSafeSig2_3 = transactionSafe2_3.getSignature(DAO_ADDRESS!) as EthSafeSignature
     // 4. Safe 트랜잭션 해시 생성
     // 트랜잭션 해시 생성
-    const txHash = await mainSafeKit.getTransactionHash(safeTransaction);
-    console.log("Safe Transaction Hash:", txHash);
+    // const txHash = await mainSafeKit.getTransactionHash(safeTransaction);
+    // console.log("Safe Transaction Hash:", txHash);
+
+    const safeTxHash = "0x34148392eddee2686a39b6da312a95afdbf953bef85122e5b0c73f3b624cba8f"
+    // const messageHash = "0x6370ebc01fb12eade1b75e7c107e2872d7a97f6cbc364514189f7da6b049d6fb"
 
     const signatures = await createMultipleSignatures(
         [multiSigOwner1, multiSigOwner2],
-        txHash
+        safeTransactionHash
     );
+
+    console.log("signerSafeSig2_3 :", signerSafeSig2_3)
     console.log("signatures :", signatures)
 
-    const result = await daoCommitteeV2.isValidSignature(txHash, signatures);
-    console.log(result)
+    // console.log("safeTransactionHash :", safeTransactionHash)
+    // console.log("safeTxHash :", safeTxHash)
+
+    const result = await daoCommitteeV2.isValidSignature(safeTransactionHash, signatures);
+    // console.log(result)
     if (result == MAGIC_VALUE) {
-        console.log("pass the Sign")
+        console.log("pass the DAOContract signature")
     } else {
         console.log("fail the Sign")
     }
@@ -144,9 +200,14 @@ async function main(): Promise<void> {
     });
 
     await apiKit.confirmTransaction(
-        txHash,
-        signatures
-    );
+        safeTransactionHash,
+        buildSignatureBytes([signerSafeSig2_3])
+    )
+
+    // await apiKit.confirmTransaction(
+    //     txHash,
+    //     signatures
+    // );
     console.log("Transaction confirmed with DAO approval!");
 
     
