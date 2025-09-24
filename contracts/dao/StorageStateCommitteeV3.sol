0bf8f7bcfa3d1e6cbd6c7b8df5783c9df64cbc94
HyukSang <zzooppii@gmail.com>
2025-09-24 14:02:39 +0900

    Update StorageStateCommitteeV3.sol

--- contracts/dao/StorageStateCommitteeV3.sol
+++ contracts/dao/StorageStateCommitteeV3.sol
@@ -6,11 +6,4 @@ contract StorageStateCommitteeV3 {
 
     // EIP-1271 관련 상태 변수들
     address public multiSigWallet; // DAO Owner (DEFAULT_ADMIN_ROLE)
-
-    uint256 constant public threshold = 1;
-
-    mapping(address => mapping(bytes32 => uint256)) public approvedHashes;
-    mapping(address => address) internal owners;
-    mapping(bytes32 => uint256) public signedMessages;
-
 }
