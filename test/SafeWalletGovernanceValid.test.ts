a8660c46388abc91d0bd4ab15178f1d3dfd704bc
HyukSang <zzooppii@gmail.com>
2025-12-01 18:50:23 +0900

    Update SafeWalletGovernanceValid.test.ts

--- test/SafeWalletGovernanceValid.test.ts
+++ test/SafeWalletGovernanceValid.test.ts
@@ -285,6 +285,32 @@ describe("EIP-1271 Upgrade Integration Tests", function () {
 
       const result = await daoCommitteeV2.callStatic.isValidSignature(txHashData, makeSignature);
       expect(result).to.equal(MAGIC_VALUE);
+
+      const transaction = await apiKit.getTransaction(
+        testHash
+      )
+      console.log("transaction", transaction)
+
+      const orginSign = await protocolKit
+        .toSafeTransactionType(transaction)
+        .then((safeTx) => Array.from(safeTx.signatures.values())[0])
+
+      console.log("orginSign", orginSign)
+
+      let sumSignature = buildSignatureBytes([
+        orginSign,
+        safeTx.getSignature(DAO_COMMITTEE_PROXY!) as SafeSignature,
+      ])
+
+      console.log("sumSignature", sumSignature)
+      console.log("sumSignature.length", sumSignature.length)
+
+
+      const signatureResponse = await apiKit.confirmTransaction(
+        safeTxHash,
+        sumSignature
+      )
+      console.log("signatureResponse", signatureResponse)
     });
 
     it("If the number of duplicate signers in isValidSignature is too small, it fails.", async function () {
