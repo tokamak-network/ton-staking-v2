812fa28905a4f153afd1931bff6f431907a1efef
HyukSang <zzooppii@gmail.com>
2025-12-10 16:33:14 +0900

    Update the contract to match the main branch, ton-staking-v2.
    The environment changed to foundry is different from the latest version, so the contracts are updated.

--- src/stake/interfaces/IWTON.sol
+++ src/stake/interfaces/IWTON.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.4;
 interface IWTON {
      function swapFromTON(uint256 tonAmount) external returns (bool);
      function swapToTON(uint256 wtonAmount) external returns (bool);
+     function ton() external view returns (address);
 }
