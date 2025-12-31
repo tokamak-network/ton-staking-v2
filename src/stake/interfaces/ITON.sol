c8392d6d0659784f02d492cb26751e8afbe164e5
HyukSang <zzooppii@gmail.com>
2025-12-31 20:17:37 +0900

    Update ITON.sol

--- src/stake/interfaces/ITON.sol
+++ src/stake/interfaces/ITON.sol
@@ -7,4 +7,5 @@ interface ITON {
     function approveAndCall(address spender, uint256 amount, bytes memory data) external returns (bool);
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
 }
