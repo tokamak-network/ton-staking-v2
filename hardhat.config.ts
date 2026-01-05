d3585fa94dbf2aed9e96dce05c7162125e28f87d
Zena-park <zena@tokamak.network>
2026-01-05 16:55:56 +0900

    chore: Update configuration files
    - Update .gitignore for Hardhat and TypeScript
    - Update foundry.toml configuration
    - Update hardhat.config.ts settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,14 +3,31 @@ import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.25",
-    settings: {
-      viaIR: true,
-      optimizer: {
-        enabled: true,
-        runs: 50,
+    compilers: [
+      {
+        version: "0.5.12",
       },
-    },
+      {
+        version: "0.8.19",
+        settings: {
+          // evmVersion: "cancun",
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+            // details: {
+            //   yul: true,
+            // },
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.8.12/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+    ],
   },
   paths: {
     sources: "./src",
